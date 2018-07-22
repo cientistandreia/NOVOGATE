8eb545e5d5c19c47a87abddd50c4c1309c6e55b3
Andrea Zanete <andreia.zanette@hotmail.com>
2018-07-22 13:02:33 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -48,19 +48,6 @@ namespace gateTest
         }
 
     }
-    //    static class Program
-    //{
-    //    /// <summary>
-    //    /// Ponto de entrada principal para o aplicativo.
-    //    /// </summary>
-    //    [STAThread]
-    //    static void Main()
-    //    {
-    //        Application.EnableVisualStyles();
-    //        Application.SetCompatibleTextRenderingDefault(false);
-    //        Application.Run(new Form1());
-    //    }
-    //}
 }
 
 
