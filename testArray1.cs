989ea32765318fc084cbbbfe86e33d2738da563d
Andrea Zanete <andreia.zanette@hotmail.com>
2018-07-22 22:47:26 -0300

    Add files via upload

---
+++ testArray1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+static class Program
+{
+    static void Main()
+    {
+
+        int[] numeros = ParseNumberFile("agenda.txt");
+    }
+
+    static int[] ParseNumberFile(string filename)
+    {
+        string fileContent = File.ReadAllText(filename);
+
+        string[] integerStrings = fileContent.Split(new char[] { ' ', '\t', '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+        int[] integers = new int[integerStrings.Length];
+
+        for (int n = 0; n < integerStrings.Length; n++)
+            integers[n] = int.Parse(integerStrings[n]);
+
+
+
+        return integers;
+    }
+
+}
